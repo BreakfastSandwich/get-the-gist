b3da8d68afae0256fde230f276a583c55ba88cda
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-11-06 18:03:31 -0500

    basic React Build

---
+++ React Intro/src/components/homepage.jsx
@@ -0,0 +1,14 @@
+import '../styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div>
+    <header>
+      <h1>Will Speakman</h1>
+    </header>
+
+   </div>
+  );
+}
+
+export default HomePage;
